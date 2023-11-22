e94e30c0cfa9113167e471b8e15c032eaba30e88
Abhishek Dinesan <abhidines04@gmail.com>
2023-11-22 13:13:40 -0500

    Added a constant/testing

--- main.cc
+++ main.cc
@@ -3,14 +3,18 @@
 #include <sstream>
 // ADD INCLUDE STATEMENTS
 using namespace std;
+const int MAX_DIFFICULTY;
 
-int main() {
+int main()
+{
     string cmd;
-    while (true) {
+    while (true)
+    {
         cin >> cmd;
-        
-        // GAME 
-        if (cmd == "game") {
+
+        // GAME
+        if (cmd == "game")
+        {
             string input = "";
             string playerType = "";
             string strCpuDifficulty;
@@ -18,30 +22,36 @@ int main() {
             bool isWhiteInit = false;
             bool isBlackInit = false;
 
+            while (!isWhiteInit || !isBlackInit)
+            {
 
-            while (!isWhiteInit || !isBlackInit) {
-                
                 getline(cin, playerType, '[');
-                
-                if (playerType == "computer") {
-                     
+
+                if (playerType == "computer")
+                {
+
                     getline(cin, strCpuDifficulty, ']');
                     // CONVERT STRING TO INT AND STORE IT
 
-                    if (cpuDifficulty > 4 || cpuDifficulty < 0) {
-                        if (!isWhiteInit) cerr << "Invalid White Player Computer Difficulty" << endl;
-                        else cerr << "Invalid Black Player Computer Difficulty" << endl;
+                    if (cpuDifficulty > MAX_DIFFICULTY || cpuDifficulty < 0)
+                    {
+                        if (!isWhiteInit)
+                            cerr << "Invalid White Player Computer Difficulty" << endl;
+                        else
+                            cerr << "Invalid Black Player Computer Difficulty" << endl;
                         break;
-                    } 
+                    }
 
-
-                    if (!isWhiteInit) {
+                    if (!isWhiteInit)
+                    {
                         //
                         // IMPLEMENT WHITE PLAYER INITIALIZATION AS A COMPUTER
                         // WITH cpuDifficulty DIFFICULTY
                         //
                         isWhiteInit = true;
-                    } else {
+                    }
+                    else
+                    {
                         //
                         // IMPLEMENT BLACK PLAYER INITIALIZATION AS A COMPUTER
                         // WITH cpuDifficulty DIFFICULTY
@@ -49,73 +59,91 @@ int main() {
                         isBlackInit = true;
                     }
                     cin.ignore(); // removes the "]" from the input
-                } else if (playerType == "human") {
-                    if (!isWhiteInit) {
+                }
+                else if (playerType == "human")
+                {
+                    if (!isWhiteInit)
+                    {
                         //
                         // IMPLEMENT WHITE PLAYER INITIALIZATION AS A HUMAN
                         //
                         isWhiteInit = true;
-                    } else {
+                    }
+                    else
+                    {
                         //
                         // IMPLEMENT BLACK PLAYER INITIALIZATION AS A HUMAN
                         //
                         isBlackInit = true;
                     }
-                } else {
-                    if (!isWhiteInit) cerr << "Invalid White Player Type" << endl;
-                    else cerr << "Invalid Black Player Type" << endl;
+                }
+                else
+                {
+                    if (!isWhiteInit)
+                        cerr << "Invalid White Player Type" << endl;
+                    else
+                        cerr << "Invalid Black Player Type" << endl;
                 }
             }
-        } 
-        
+        }
+
         // RESIGN
-        else if (cmd == "resign") {
-            // 
+        else if (cmd == "resign")
+        {
+            //
             // IMPLEMENT RESIGNATION
             //
-        } 
-        
+        }
+
         // MOVE
-        else if (cmd == "move") {
+        else if (cmd == "move")
+        {
             char startFile = 'a';
             int startRank = 1;
             char endFile = 'h';
             int endRank = 8;
             bool validSquares = true;
 
-            if (startFile < 'a' || startFile > 'h') {
+            if (startFile < 'a' || startFile > 'h')
+            {
                 cerr << "Invalid Starting File" << endl;
                 validSquares = false;
             }
 
-            if (startRank < 1 || startRank > 8) {
+            if (startRank < 1 || startRank > 8)
+            {
                 cerr << "Invalid Starting Rank" << endl;
                 validSquares = false;
             }
 
-            if (endFile < 'a' || startFile > 'h') {
+            if (endFile < 'a' || startFile > 'h')
+            {
                 cerr << "Invalid Ending File" << endl;
                 validSquares = false;
             }
 
-            if (endRank < 1 || startRank > 8) {
+            if (endRank < 1 || startRank > 8)
+            {
                 cerr << "Invalid Ending Rank" << endl;
                 validSquares = false;
             }
 
-            if (validSquares) {
+            if (validSquares)
+            {
                 //
                 // IMPLEMENT THE MOVE
                 //
             }
-        } 
-        
+        }
+
         // SETUP
-        else if (cmd == "setup") {
+        else if (cmd == "setup")
+        {
             //
             // IMPLEMENT LOGIC CHECKING IF A GAME IS CURRENTLY RUNNING
-            // 
-            while (true) {
+            //
+            while (true)
+            {
                 string oper;
                 string colour;
                 char piece = 'K';
@@ -123,30 +151,36 @@ int main() {
                 int rank = 1;
 
                 cin >> oper;
-                if (oper == "done") {
+                if (oper == "done")
+                {
                     break;
-                } else if (oper == "+") {
+                }
+                else if (oper == "+")
+                {
                     cin >> piece;
-                    if (piece != 'K' && piece != 'k' && 
-                        piece != 'Q' && piece != 'q' && 
+                    if (piece != 'K' && piece != 'k' &&
+                        piece != 'Q' && piece != 'q' &&
                         piece != 'B' && piece != 'b' &&
                         piece != 'N' && piece != 'n' &&
                         piece != 'R' && piece != 'r' &&
-                        piece != 'P' && piece != 'p') {
-                            cerr << "Invalid Piece" << endl;
-                            cin.clear();
-                            continue;
-                        }
+                        piece != 'P' && piece != 'p')
+                    {
+                        cerr << "Invalid Piece" << endl;
+                        cin.clear();
+                        continue;
+                    }
 
-                        cin >> file;
-                    if (file < 'a' || file > 'h') {
+                    cin >> file;
+                    if (file < 'a' || file > 'h')
+                    {
                         cerr << "Invalid File" << endl;
                         cin.clear();
                         continue;
                     }
 
                     cin >> rank;
-                    if (rank < 1 || rank > 8) {
+                    if (rank < 1 || rank > 8)
+                    {
                         cerr << "Invalid Rank" << endl;
                         cin.clear();
                         continue;
@@ -155,17 +189,21 @@ int main() {
                     //
                     // IMPLEMENT THE ADDITION OF A PIECE TO THE BOARD
                     //
-                } else if (oper == "-") {
+                }
+                else if (oper == "-")
+                {
 
-                       cin >> file;
-                    if (file < 'a' || file > 'h') {
+                    cin >> file;
+                    if (file < 'a' || file > 'h')
+                    {
                         cerr << "Invalid File" << endl;
                         cin.clear();
                         continue;
                     }
 
                     cin >> rank;
-                    if (rank < 1 || rank > 8) {
+                    if (rank < 1 || rank > 8)
+                    {
                         cerr << "Invalid Rank" << endl;
                         cin.clear();
                         continue;
@@ -174,10 +212,12 @@ int main() {
                     //
                     // IMPLEMENT THE REMOVAL OF A PIECE FROM THE BOARD
                     //
-
-                } else if (oper == "=") {
-                    cin >> colour; 
-                    if (colour != "white" && colour != "black") {
+                }
+                else if (oper == "=")
+                {
+                    cin >> colour;
+                    if (colour != "white" && colour != "black")
+                    {
                         cerr << "Invalid Piece" << endl;
                         cin.clear();
                         continue;
@@ -186,7 +226,6 @@ int main() {
                     //
                     // SET THE INDICATED PLAYERS TURN
                     //
-
                 }
             }
             //
