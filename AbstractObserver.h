f7030e4e18581cd27bc8cc34734191a15b4cb694
Abhishek Dinesan <abhidines04@gmail.com>
2023-11-27 23:01:20 -0500

    more implementaion

--- AbstractObserver.h
+++ AbstractObserver.h
@@ -1,14 +1,19 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
 
-template<typename T>
-class Observer {
-    public:
-        virtual void notify(T &obj) = 0;  // obj is the object that called the notify method
-        virtual SubscriptionType subType() = 0;
-        virtual ~Observer() = default;
+template <typename T>
+class Observer
+{
+public:
+    virtual void notify(T &obj) = 0; // obj is the object that called the notify method
+    virtual SubscriptionType subType() = 0;
+    virtual ~Observer() = default;
 };
 
-enum class SubscriptionType { All, MoveOnly };
+enum class SubscriptionType
+{
+    All,
+    MoveOnly
+};
 
-#endif 
+#endif
