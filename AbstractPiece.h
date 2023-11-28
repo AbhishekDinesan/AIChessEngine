ed476872dc4ca75f53eadeec296374c3046fdb10
Anthony Fuca <ajfuca@gmail.com>
2023-11-28 12:40:47 -0500

    Added the Piece Enum Class

--- AbstractPiece.h
+++ AbstractPiece.h
@@ -1,13 +1,13 @@
-/*
 
 #ifndef _ABSTRACTPIECE_H_
 #define _ABSTRACTPIECE_H_
 
+enum class PieceEnum { None, Pawn, Rook, Knight, Bishop, Queen, King };
+
 class Piece {
     bool isWhite;
     bool isAlive;
-    struct position {}
-    Move nextMove {}
+    Move nextMove {};
  public:
   virtual void isValidMove(Move &m) = 0;
   virtual bool getColour() = 0;
@@ -16,5 +16,5 @@ class Piece {
   virtual int getY() = 0;
   virtual ~Piece() = default;
 };
+
 #endif
-*/
