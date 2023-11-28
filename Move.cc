e4f6ca37eea68658be86ecd6d1b34e0ecd991232
Anthony Fuca <ajfuca@gmail.com>
2023-11-28 17:39:02 -0500

    Fixed-NonePiece-Issues

---
+++ Move.cc
@@ -0,0 +1,8 @@
+#include "Move.h"
+
+Move::Move() {}
+Move::~Move() {}
+Piece* Move::CapturedPiece() {}
+bool Move::isCheck() { return false; }
+void Move::UpdateBoard() {}
+bool Move::willCheck() { return false; }
