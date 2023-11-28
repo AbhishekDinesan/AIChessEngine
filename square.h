16e8136ec82e6f21da4f899f97301cdd89c2e078
Satya Patel <satyapatel2004@icloud.com>
2023-11-28 14:02:51 -0500

    Added an implementation for Square

--- square.h
+++ square.h
@@ -3,17 +3,34 @@
 #ifndef __SQUARE_H__
 #define __SQUARE_H__
 
-class Square
-{
+#include "AbstractObserver.h"
+#include "AbstractPiece.h"
+#include <vector> 
+
+ 
+//NOTE: Square is not an observer, it will be observed though
+//      (by textdisplay, graphicsdisplay)
+
+class Square {
     int x;
     int y;
+    bool occupied; 
+    std::vector<Observer *> observers; 
+    
+    Piece *occupiedBy;  
 
 public:
+
     Square();
     ~Square();
 
     int getX();
     int getY();
+    bool getOccupied(); 
+    Piece *getOccupyingPc();
+
+    void notifyObservers(); 
+    void attach(Observer *o); 
 };
 
 #endif
