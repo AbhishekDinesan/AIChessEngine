fa641ec600b316b97c15ecf29f0cb25343479162
Abhishek Dinesan <abhidines04@gmail.com>
2023-11-30 20:51:57 -0500

    I need this #includes to compile idk bout you guys

--- Game.h
+++ Game.h
@@ -7,12 +7,13 @@
 #include "AbstractPlayer.h"
 #include "Player.h"
 #include "textdisplay.h"
+#include <memory>
 using namespace std;
 
 // class Board; forward declerations
 // class Player;
 
-using namespace std;  
+using namespace std;
 
 class Game
 {
@@ -21,7 +22,7 @@ class Game
     //  Player currentPlayer;
     int moveCount;
     // Vector pastMoves;
-    unique_ptr<Board> board; 
+    unique_ptr<Board> board;
 
 public:
     Game();
@@ -30,13 +31,11 @@ public:
     void init();
     bool ValidBoard();
     bool endGame();
-    
-    
+
     void printBoard();
-    std::unique_ptr<Board>& getBoard(); 
+    std::unique_ptr<Board> &getBoard();
     void addPiece(char piece, int x, int y);
     friend std::ostream &operator<<(std::ostream &out, const Game &g);
- 
 };
 
 #endif
