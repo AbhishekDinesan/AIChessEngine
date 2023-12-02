af877e6d3b39c148269d11ebf450cf52c00fb266
Abhishek Dinesan <abhidines04@gmail.com>
2023-12-01 21:27:31 -0500

    Added ComputerAbstract/Concretes

---
+++ ComputerFour.cc
@@ -0,0 +1,11 @@
+#include "AbstractComputer.h"
+
+class ComputerFour : public Computer
+{
+public:
+    void makeMove(Move &m) override
+    {
+        // Implement logic for difficulty level 0
+        // ...
+    }
+};
